4bdc3b9098078c3a96a88a808ffd5ac11338ccac
Владимир Долгов <Barakyl@outlook.com>
2020-12-11 21:33:01 +0300

    Создание и подключение классов

--- IngameScript/Program.cs
+++ IngameScript/Program.cs
@@ -2,81 +2,124 @@
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
-using VRage;
+using System;
 using VRage.Collections;
-using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.GUI.TextPanel;
-using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
 using VRageMath;
 
 namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        // ============  ОБЯЗАТЕЛЬНЫЕ НАСТРОЙКИ ============ 
+        const string storeName = "Магазин компонентов"; // Имя блока магазина (если не указано то будет выбран первый подходящий в гирде)
+
+        // ============ ОПЦИОНАЛЬНЫЕ НАСТРОЙКИ МАГАЗИНА ============ 
+        const int timeRefresh = 3600; // Интервал для обновления товаров в магазине в секундах (3600 сек = 1 час)
+
+        // Тег в названии блока для исключения из работы скрипта
+        const string tagExclude = "Исключить";
+
+        // Имя группы контейнеров для проверки ресурсов и торговли (заранее создать группу)
+        const string groupContainersForTrade = "";
+
+        // Тег в названии контейнера для проверки на наличие ресурсов и торговли
+        const string tagContainerForTrade = "";
+
+        // Список компонентов (меняем только указанные цены и закупку/продажу. Названия не трогать!)
+        static internal Dictionary<string, MyItem> Components = new Dictionary<string, MyItem>()
+        {// Граница закупки и продажи / Цена закупки / Вкл закупку / Цена продажи / Вкл продажу
+            ["BulletproofGlass"] = new MyItem(235, 830, true, 1065, true),// Бронестекло
+            ["Canvas"] = new MyItem(10, 2500, true, 3105, true),// Парашут
+            ["Computer"] = new MyItem(400, 45, true, 47, true),// Компьютеры
+            ["Construction"] = new MyItem(1000, 430, true, 498, true),// Строительные компоненты
+            ["Detector"] = new MyItem(30, 2236, true, 2743, true),// Компоненты детектора
+            ["Display"] = new MyItem(30, 381, true, 416, true),// Экраны
+            ["Explosives"] = new MyItem(0, 33633, false, 37475, false),// Взрывчатка
+            ["Girder"] = new MyItem(100, 360, true, 374, true),// Балка
+            ["GravityGenerator"] = new MyItem(0, 150000, false, 385875, false),// Компоненты гравигенератора
+            ["InteriorPlate"] = new MyItem(200, 154, true, 187, true),// Внутренние пластины
+            ["LargeTube"] = new MyItem(200, 1702, true, 2079, true),// Большие трубы
+            ["Medical"] = new MyItem(0, 40000, false, 43072, false),// Медицинские компоненты
+            ["MetalGrid"] = new MyItem(300, 3265, true, 3625, true),// Компоненты решетки
+            ["Motor"] = new MyItem(150, 2008, true, 2228, true),// Моторы
+            ["PowerCell"] = new MyItem(50, 1078, true, 1180, true),// Батарейки
+            ["RadioCommunication"] = new MyItem(30, 515, true, 710, true),//Радиоантенна
+            ["Reactor"] = new MyItem(0, 6410, false, 8478, false),// Компоненты реактора
+            ["SmallTube"] = new MyItem(300, 267, true, 311, true),// Малые трубки
+            ["SolarCell"] = new MyItem(0, 641, false, 849, false),// Солнечные панели
+            ["SteelPlate"] = new MyItem(2500, 1236, true, 1311, true),// Стальные пластины
+            ["Superconductor"] = new MyItem(0, 21354, false, 26524, false),// Сверхпроводник
+            ["Thrust"] = new MyItem(0, 41325, false, 45068, false),// Компоненты двигателей
+            ["ZoneChip"] = new MyItem(0, 105000, false, 100000, false),// Ключи
+        };
+
+        // ============ КОНЕЦ НАСТРОЕК ============
+        MyAutoStore AutoStore;
+        readonly string[] arguments = new string[] {
+            "магазин.разместить",
+            "магазин.очистить",
+            "магазин.список"
+        };
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
+            AutoStore = new MyAutoStore(GridTerminalSystem, Me.CubeGrid, storeName, timeRefresh);
+            if (AutoStore.StoreComp.Block != null)
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.Update10;
+                Echo("Скрипт запущен\n"); AvailableCommands();
+            }
+            else Echo("Магазин не найден\nВыполнение остановлено");
         }
 
-        public void Save()
+        public void Main(string arg, UpdateType updateSource)
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            string txt = $"Время выполнения: {Math.Round(Runtime.LastRunTimeMs, 3)}";
+            txt += $"\nОбновление магазина через {AutoStore.TimeCheckStore.RestTime} сек";
+            if (AutoStore.TimeToUpgrade()) // Ожидаем, пока не настанет время обновления магазина
+                AutoStore.StoreUpdate(GridTerminalSystem, Me, groupContainersForTrade, tagContainerForTrade, tagExclude); // Главный метод обновления товаров в магазине
+            Me.GetSurface(0).WriteText(txt);
+            if (arg != string.Empty) Arguments(arg);
         }
 
-        public void Main(string argument, UpdateType updateSource)
+        void Arguments(string arg)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            if (arg.ToLower() == arguments[0])
+            {
+                AutoStore.TimeCheckStore.Stop();
+                Echo(" => \nРазмещение товаров");
+                AvailableCommands();
+            }
+            else if (arg.ToLower() == arguments[1])
+            {
+                AutoStore.StoreComp.ClearAll();
+                Echo(" => \nОчистка завершено");
+                AvailableCommands();
+            }
+            else if (arg.ToLower() == arguments[2])
+            {
+                Me.CustomData = AutoStore.StoreComp.GetOrdersAndOffers();
+                Echo(" => \nТовары из магазина\nвыведны в данные ПБ");
+                AvailableCommands();
+            }
+            else AvailableCommands();
+        }
+        void AvailableCommands()
+        {
+            string info = "\nВозможные аргументы:";
+            foreach (var arg in arguments) { info += $"\n{arg}"; }
+            Echo(info);
         }
     }
 }
---
+++ Mixins/MyProductStore/MyProductStore.cs
@@ -0,0 +1,179 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class MyProductStore
+        {
+            internal IMyStoreBlock Block { get; private set; }
+
+            /// <summary>
+            /// Поиск необходимых блоков
+            /// </summary>
+            /// <param name="terminalSystem">IMyGridTerminalSystem</param>
+            /// <param name="ThisCubeGrid">Грид, на котором установлен магазин</param>
+            /// <param name="nameStore">Имя блока магазина</param>
+            internal MyProductStore(IMyStoreBlock StoreBlock)
+            {
+                Block = StoreBlock;
+            }
+            internal MyProductStore(IMyGridTerminalSystem TerminalSystem, IMyCubeGrid CubeGrid, string nameStore)
+            {
+                GetBlocks(TerminalSystem, CubeGrid, nameStore);
+            }
+            internal void GetBlocks(IMyGridTerminalSystem TerminalSystem, IMyCubeGrid ThisCubeGrid, string nameStore)
+            {
+                Block = TerminalSystem.GetBlockWithName(nameStore) as IMyStoreBlock;
+                if (Block == null)
+                {
+                    List<IMyStoreBlock> temp = new List<IMyStoreBlock>();
+                    TerminalSystem.GetBlocksOfType(temp, x => x.CubeGrid == ThisCubeGrid);
+                    foreach (var t in temp) { if (t.IsWorking) Block = t; break; }
+                }
+            }
+            /// <summary>
+            /// Размещение списка товаров в магазине
+            /// </summary>
+            /// <param name="ItemsForSaleBuy"></param>
+            internal void OfferingsAndSales(Dictionary<string, MyItem> ItemsForSaleBuy)
+            {
+                if (Block != null && Block.IsWorking)
+                {
+                    List<MyStoreQueryItem> storeItems = new List<MyStoreQueryItem>();// Товары в магазине
+                    Block.CustomData = "";
+                    // Получение списка из магазина
+                    Block.GetPlayerStoreItems(storeItems);
+                    // Проверка каждого товара на докупку/продажу
+                    foreach (var Item in ItemsForSaleBuy)
+                    {
+                        // Если разрешена закупка этого товара и его мало на складе
+                        if (Item.Value.AlowBuy && Item.Value.Amount < Item.Value.MaxAmount)
+                        {
+                            int dif = Item.Value.MaxAmount - Item.Value.Amount; // Кол-во недостающего товара
+                            if (!OfferOrderIsPosted(Item.Key, Item.Value.BuyPrice, dif, storeItems)) // Если такое объявление еще не создавали
+                            {
+                                FindAndDelete(Item.Key, storeItems); // Удаляем похожие объявления
+                                InsertOrder("MyObjectBuilder_Component/" + Item.Key, dif, Item.Value.BuyPrice); // Создаём ордер на закупку
+                            }
+                            else Block.CustomData += $"\n[No update] Закупка {Item.Key}:{dif} шт по цене {Item.Value.BuyPrice} кр. уже размещена";
+                        }
+                        // Если разрешена продажа этого товара и его много на складе
+                        else if (Item.Value.AlowSale && Item.Value.Amount > Item.Value.MaxAmount)
+                        {
+                            int dif = Item.Value.Amount - Item.Value.MaxAmount; // Кол-во лишнего товара
+                            if (!OfferOrderIsPosted(Item.Key, Item.Value.SalePrice, dif, storeItems)) // Если такое объявление еще не создавали
+                            {
+                                FindAndDelete(Item.Key, storeItems); // Удаляем похожие объявления
+                                InsertOffer("MyObjectBuilder_Component/" + Item.Key, dif, Item.Value.SalePrice); // Создаём ордер на продажу
+                            }
+                            else Block.CustomData += $"\n[No update] Продажа {Item.Key}:{dif} шт по цене {Item.Value.BuyPrice} кр. уже размещена";
+                        }
+                    }
+                    storeItems.Clear();
+                }
+            }
+            /// <summary>
+            /// Поиск и удаление объявления о продаже из магазина
+            /// </summary>
+            /// <param name="SubtypeId">Название товара, ввиде "SteelPlate"</param>
+            /// <param name="storeItems">Список объявлений из магазина</param>
+            internal void FindAndDelete(string SubtypeId, List<MyStoreQueryItem> storeItems)
+            {
+                foreach (var item in storeItems) { if (item.ItemId.SubtypeId == SubtypeId) { Block.CustomData += $"\n[Remove] Товар {item.ItemId.SubtypeId} снят"; Block.CancelStoreItem(item.Id); } }
+            }
+            /// <summary>
+            /// Определяет содержится ли такое объявление в списке
+            /// </summary>
+            /// <param name="SubtypeId">Название товара, ввиде "SteelPlate"</param>
+            /// <param name="price">Цена</param>
+            /// <param name="amount">Колличество</param>
+            /// <param name="storeItems">Список объявлений из магазина</param>
+            /// <returns>Возвраает true, если объявление найдено</returns>
+            internal bool OfferOrderIsPosted(string SubtypeId, int price, int amount, List<MyStoreQueryItem> storeItems)
+            {
+                return storeItems.Exists(x => x.ItemId.SubtypeId == SubtypeId && x.PricePerUnit == price && x.Amount == amount);
+            }
+            /// <summary>
+            /// Создаёт предложение для закупки в блоке магазина
+            /// </summary>
+            /// <param name="itemType">Тип объекта</param>
+            /// <param name="amount">Колличество</param>
+            /// <param name="price">Цена</param>
+            /// <param name="orderId">ID заказа в магазине</param>
+            internal void InsertOrder(string itemType, int amount, int price)
+            {
+                long orderId = 0;
+                MyDefinitionId definitionId;
+                if (MyDefinitionId.TryParse(itemType, out definitionId))
+                {
+                    Block.InsertOrder(new MyStoreItemDataSimple(definitionId, amount, price), out orderId); // Мы закупаем
+                }
+                else Block.CustomData += $"\nОШИБКА [MyDefinitionId] НЕ создан. Вероятно указан не правильный itemType";
+                if (orderId != 0) Block.CustomData += $"\n[Create] Создан заказ [{itemType}] {amount} шт по цене {price}";
+                else Block.CustomData += $"\nОШИБКА! Заказ [{itemType}] не создан. Проверьте имя товара";
+            }
+            /// <summary>
+            /// Создаёт предложение для продажи в блоке магазина
+            /// </summary>
+            /// <param name="itemType">Тип объекта</param>
+            /// <param name="amount">Колличество</param>
+            /// <param name="price">Цена</param>
+            internal void InsertOffer(string itemType, int amount, int price)
+            {
+                long orderId = 0;
+                MyDefinitionId definitionId;
+                if (MyDefinitionId.TryParse(itemType, out definitionId))
+                {
+                    Block.InsertOffer(new MyStoreItemDataSimple(definitionId, amount, price), out orderId); // Мы закупаем
+                }
+                else Block.CustomData += $"\n[Error] [MyDefinitionId] НЕ создан. Вероятно указан не правильный itemType";
+                if (orderId != 0) Block.CustomData += $"\n[Create] Создано предложение продажи [{itemType}] {amount} шт по цене {price}";
+                else Block.CustomData += $"\n[Error]! Заказ [{itemType}] не создан.Проверьте имя и цену[{price}]";
+            }
+            /// <summary>
+            /// Получить список заказов и предложений из магазина
+            /// </summary>
+            /// <returns>Возвращает заказы и предложения в текстовом виде</returns>
+            internal string GetOrdersAndOffers()
+            {
+                List<MyStoreQueryItem> storeItems = new List<MyStoreQueryItem>();// Товары в магазине
+                string txt;
+                Block.GetPlayerStoreItems(storeItems);
+                txt = $"Товаров в магазине {storeItems.Count}";
+                foreach (var item in storeItems)
+                {
+                    txt += $"\n{item.ItemId.SubtypeId} {item.Amount} шт по цене {item.PricePerUnit}";
+                }
+                return txt;
+            }
+            /// <summary>
+            /// Очистка всех заказов и предложений в магазине
+            /// </summary>
+            internal void ClearAll()
+            {
+                List<MyStoreQueryItem> storeItems = new List<MyStoreQueryItem>();// Товары в магазине
+                Block.CustomData = "Очистка магазина";
+                Block.GetPlayerStoreItems(storeItems);
+                foreach (var item in storeItems) { Block.CancelStoreItem(item.Id); }
+                Block.CustomData += $"\n..удалено {storeItems.Count} позиций";
+                storeItems.Clear();
+            }
+        }
+    }
+}
